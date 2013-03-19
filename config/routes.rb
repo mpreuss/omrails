d9e5a71c553152e8d8e8ddfd32e5b31021d87af1
Michael Preuss <mike.preuss@gmail.com>
2013-03-19 16:43:20 -0500

    Adding Devise Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Omrails::Application.routes.draw do
+  devise_for :users
+
   get 'about' => 'pages#about'
 
-  get 'golfrator' => 'pages#golfrator'
+  get 'golfrater' => 'pages#golfrater'
 
   root :to => 'pages#home'
 
