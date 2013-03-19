b1d1acc3bee19cb1aed377787fe8c50640dd0a31
Michael Preuss <mike.preuss@gmail.com>
2013-03-19 15:32:55 -0500

    Adding Golfrator Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Omrails::Application.routes.draw do
   get 'about' => 'pages#about'
 
+  get 'golfrator' => 'pages#golfrator'
+
   root :to => 'pages#home'
 
   # The priority is based upon order of creation:
