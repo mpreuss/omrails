91b4e438af129da7e8a0e187032a479d9aba3c53
Michael Preuss <mike.preuss@gmail.com>
2013-04-09 11:22:23 -0700

    Fix Beeliner Route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Omrails::Application.routes.draw do
 
   get 'about' => 'pages#about'
   get 'golfrater' => 'pages#golfrater'
-  get 'beeliber' => 'pages#beeliner'
+  get 'beeliner' => 'pages#beeliner'
 
   root :to => 'pins#index'
 
